422b21b95c300593fbe4cef199725edf15372418
Robinvsa <robinvsa@hotmail.nl>
2019-04-05 08:41:46 +0200

    Quiz 3 H10 test

---
+++ src/Q3H10MijnKlasse.java
@@ -0,0 +1,19 @@
+public class Q3H10MijnKlasse(){
+    static int x = 6;
+    int y = 3;
+
+    MijnKlasse(){
+        x+=3;
+        y+=2;
+        }
+        void mijnMethode(int i){
+        this.y = y-i;
+        x++
+        }
+
+    public static void main(String[]args){
+        MijnKlasse mk1 = new MijnKlasse();
+
+        }
+}
+
